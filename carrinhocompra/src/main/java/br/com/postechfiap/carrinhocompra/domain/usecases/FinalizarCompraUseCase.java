342b610c4bdc4db202b37450cb6571e1737e800f
Igor Monte <igor.monte1@hotmail.com>
2024-07-22 00:36:29 -0300

    Remoção de Caso de Uso não utilizado

--- carrinhocompra/src/main/java/br/com/postechfiap/carrinhocompra/domain/usecases/FinalizarCompraUseCase.java
+++
@@ -1,9 +0,0 @@
-package br.com.postechfiap.carrinhocompra.domain.usecases;
-
-import br.com.postechfiap.carrinhocompra.domain.entity.CarrinhoCompra;
-
-import java.util.UUID;
-
-public interface FinalizarCompraUseCase {
-    CarrinhoCompra executar(UUID idCarrinho);
-}
--- carrinhocompra/src/main/java/br/com/postechfiap/carrinhocompra/domain/usecases/repository/FinalizarCompraRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.postechfiap.carrinhocompra.domain.usecases.repository;
-
-import br.com.postechfiap.carrinhocompra.domain.entity.CarrinhoCompra;
-
-import java.util.UUID;
-
-public interface FinalizarCompraRepository {
-    CarrinhoCompra executar(UUID idCarrinho);
-}
